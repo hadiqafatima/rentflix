a969325d764a08331b5de877dae76ba2decb5b56
= <=>
2018-12-22 19:19:09 +0500

    Restful api customer

---
+++ Rentflix/Rentflix/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Rentflix.Dtos;
+using Rentflix.Models;
+
+namespace Rentflix.App_Start
+{
+    public class MappingProfile :Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
---
+++ Rentflix/Rentflix/Controllers/Api/CustomersController.cs
@@ -0,0 +1,84 @@
+﻿using Rentflix.Dtos;
+using Rentflix.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+
+
+namespace Rentflix.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext db;
+        public CustomersController()
+        {
+            db = new ApplicationDbContext();
+        }
+        //GET api/customers
+        public IEnumerable<CustomerDto> GetCustomers()
+        {
+            return db.Customers.ToList().Select(Mapper.Map<Customer,CustomerDto>);
+        }
+        //GET api/customers/1
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = db.Customers.Single(c => c.Id == id);
+            if (customer == null)
+            {
+                NotFound();
+            }
+            return Ok(Mapper.Map<Customer,CustomerDto>(customer));
+        }
+        //POST /api/customers
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            customerDto.Id = customer.Id;
+            return Created(new Uri(Request.RequestUri+"/"+customer.Id),customerDto);
+
+        }
+
+        //PUT /api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            var customerInDB = db.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDB == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            Mapper.Map(customerDto, customerInDB);
+            db.SaveChanges();
+
+        }
+
+        //DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDB = db.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDB == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            db.Customers.Remove(customerInDB);
+            db.SaveChanges();
+
+        }
+    }
+}
